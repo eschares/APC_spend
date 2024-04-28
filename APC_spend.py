53671fb65ea27f9f4bd78bc69228e603217d213f
Eric Schares <38474105+eschares@users.noreply.github.com>
2024-04-28 18:32:43 -0500

    Add files via upload

--- APC_spend.py
+++ APC_spend.py
@@ -23,10 +23,11 @@ fig = px.scatter(merged2, x='n_works', y='Dim_count', color='issn', symbol='key'
               "n_works": "OpenAlex count",
               "Dim_count": "Dimensions count"}
         )
+fig.update_traces(marker=dict(size=10))
 
 fig.add_shape(type="line",
     x0=1, y0=0, x1=2000, y1=2000,
     line=dict(color="Purple", width=1, dash="dot"))
 
-fig.update_layout(width=1200, height=800)
+fig.update_layout(width=1000, height=600)
 st.plotly_chart(fig, use_container_width=True)
