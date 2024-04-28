f85126578c68ed97a6ba58a163d8831091ad77c3
Eric Schares <38474105+eschares@users.noreply.github.com>
2024-04-28 11:31:44 -0500

    Add files via upload

---
+++ APC_spend.py
@@ -0,0 +1,23 @@
+# Compare counts of journal and year in OpenAlex and Dimensions
+# Eric Schares, 4/28/24
+
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+merged2 = pd.read_csv('OpenAlex_and_Dimensions_counts_merged_withJournalName.csv')
+
+fig = px.scatter(merged2, x='n_works', y='Dim_count', color='issn', symbol='key', opacity=0.7,
+          title='Count of articles in OpenAlex and Dimensions, by journal/year/OA status',
+          hover_name = 'Publisher_Journalname',
+          hover_data = ['issn', 'year'],
+          labels={
+              "n_works": "OpenAlex count",
+              "Dim_count": "Dimensions count"}
+        )
+
+fig.add_shape(type="line",
+    x0=1, y0=0, x1=2000, y1=2000,
+    line=dict(color="Purple", width=1, dash="dot"))
+
+st.plotly_chart(fig)
